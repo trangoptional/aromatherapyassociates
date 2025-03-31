37e8756073db3089e067957b81a5d68f9a6f188f
le_thi_trang <le_thi_trang@shuei-infotech.net>
2025-03-31 11:54:05 +0700

    fix css path

--- load-footer.js
+++ load-footer.js
@@ -3,12 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
       .then((response) => response.text())
       .then((data) => {
         document.getElementById("footer-container").innerHTML = data;
-
-        document.querySelectorAll("style").forEach((style) => {
-          if (style.innerHTML.includes("body { background-color: #f1f1f1;")) {
-            style.remove();
-          }
-        });
       })
       .catch((error) => console.error("Lỗi tải footer:", error));
   });
