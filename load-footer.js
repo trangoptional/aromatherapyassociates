afb737d39e15b669ad3d8a86f6faf46d931849cb
le_thi_trang <le_thi_trang@shuei-infotech.net>
2025-04-01 16:15:56 +0700

    update layout

--- load-footer.js
+++ load-footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("components/footer.html")
+    fetch("/components/footer.html")
       .then((response) => response.text())
       .then((data) => {
         document.getElementById("footer-container").innerHTML = data;
