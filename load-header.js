52d4cba3621cd8f8620270d979d944e1c08f81a4
le_thi_trang <le_thi_trang@shuei-infotech.net>
2025-04-01 15:53:24 +0700

    update layout

--- load-header.js
+++ load-header.js
@@ -1,6 +1,6 @@
 // load-header.js
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/components/header.html")
+  fetch("components/header.html")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("header-container").innerHTML = data;
